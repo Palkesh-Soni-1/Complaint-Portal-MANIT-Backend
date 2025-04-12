4e569cb411bc8e0af8bc729fbcb3cd207f74a73c
Palkesh Soni <sonipalkesh43@gmail.com>
2025-04-12 14:52:11 +0530

    feat: initialize backend with Express and MongoDB connection
    - Created package.json with necessary dependencies including express, mongoose, cors, dotenv, cron, and morgan.
    - Set up server.js to configure Express server, connect to MongoDB, and define a basic route.
    - Added dotenv for environment variable management and cors for handling cross-origin requests.

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+dotenv.config();
+import cors from 'cors';
+
+connectDB();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running`);
+})
+
+
