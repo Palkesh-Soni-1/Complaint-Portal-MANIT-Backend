75c7881a87944a3f4a57de0009e0f31922e6a5f4
satya-supercluster <satya.supercluster@gmail.com>
2025-04-14 11:50:59 +0530

    feat: sort complaints by creation date in getAllComplaints and getComplaintsById controllers

--- controllers/getComplaintById.controller.js
+++ controllers/getComplaintById.controller.js
@@ -3,7 +3,9 @@ import Complaints from "../models/complaint.model.js";
 const getComplaintsByIdController = async (req,res) =>{
     const {studentId} = req.query;
     try{
-        const finalData = await Complaints.find({studentId});
+        const finalData = await Complaints.find({ studentId }).sort({
+          createdAt: -1,
+        });
         res.status(200).json({data:finalData});
     }
     catch(err){
